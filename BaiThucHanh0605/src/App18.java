4e39689344e68173dc44d05a8e905b5add4247b9
Thanh Tra <thanktea2102002@gmail.com>
2022-05-15 20:47:37 +0700

    commit note

---
+++ BaiThucHanh0605/src/App18.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class App18 {
+public static void main(String[] args){
+    ArrayList<Float> arraylistFloat = new ArrayList<>();
+    arraylistFloat.add(0.7f);
+    arraylistFloat.add(7.26f);
+    arraylistFloat.add(1.02f);
+    arraylistFloat.add(9.3f);
+    Iterator<Float> interator = arraylistFloat.iterator();
+    System.out.println("Các phần tử có trong arrayListFloat là :");
+    while (interator.hasNext()){
+        System.out.println(interator.next()+ "\t");
+    }
+}
+}
